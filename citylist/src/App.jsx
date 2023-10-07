9fdb7eddfdce351c49248e040f7e12db2cdb750c
Kristyn Taniguchi <90283966+kristyntaniguchi@users.noreply.github.com>
2023-10-06 19:39:06 -0700

    List and Details Complete

--- citylist/src/App.jsx
+++ citylist/src/App.jsx
@@ -5,6 +5,33 @@ import Details from "./Pages/Details";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navigation from "./Components/Navigation";
 import { useState, createContext } from "react";
+const cities = [
+  { id: 1, name: "New York", country: "USA", population: 8175133 },
+  { id: 2, name: "Los Angeles", country: "USA", population: 3792621 },
+  { id: 3, name: "London", country: "UK", population: 8787892 },
+  { id: 4, name: "Tokyo", country: "Japan", population: 13751518 },
+  { id: 5, name: "Shanghai", country: "China", population: 23019148 },
+  { id: 6, name: "Mumbai", country: "India", population: 12442373 },
+  { id: 7, name: "Sydney", country: "Australia", population: 4627345 },
+  { id: 8, name: "Cairo", country: "Egypt", population: 9500000 },
+  { id: 9, name: "Sao Paulo", country: "Brazil", population: 12106920 },
+  { id: 10, name: "Moscow", country: "Russia", population: 11920000 },
+  {
+    id: 11,
+    name: "Johannesburg",
+    country: "South Africa",
+    population: 4434837,
+  },
+  { id: 12, name: "Paris", country: "France", population: 2148327 },
+  { id: 13, name: "Toronto", country: "Canada", population: 2731571 },
+  { id: 14, name: "Berlin", country: "Germany", population: 3644826 },
+  { id: 15, name: "Rome", country: "Italy", population: 2870500 },
+  { id: 16, name: "Dubai", country: "UAE", population: 3137000 },
+  { id: 17, name: "Bangkok", country: "Thailand", population: 8281000 },
+  { id: 18, name: "Mexico City", country: "Mexico", population: 8918653 },
+  { id: 19, name: "Auckland", country: "New Zealand", population: 1515500 },
+  { id: 20, name: "Seoul", country: "South Korea", population: 9733509 },
+];
 
 //Context is a global state
 //It is used to pass shared data between ALL components
@@ -18,7 +45,6 @@ function App() {
     city.id = citiesList.length + 1;
     setCitiesList([...citiesList, city]);
   }
-
   return (
     <>
       <BrowserRouter>
@@ -36,5 +62,5 @@ function App() {
     </>
   );
 }
-
 export default App;
+export { CitiesContext };
--- citylist/src/Pages/Details.jsx
+++ citylist/src/Pages/Details.jsx
@@ -1,7 +1,21 @@
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { CitiesContext } from "../App";
+
 function Details() {
+  //useContext is used to access the context
+  const { citiesList } = useContext(CitiesContext);
+  //useParams is used to acess the parameters passed in the URL
+  const { id } = useParams();
+  //find the city with the id passed in the URL
+  const city = citiesList.find((city) => city.id === parseInt(id));
   return (
     <>
-      <h2>Details</h2>
+      <div className="card">
+        <h2>{city.name}</h2>
+        <p>Country: {city.country}</p>
+        <p>Population: {city.population}</p>
+      </div>
     </>
   );
 }
